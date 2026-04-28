d754a04584cde63a44b754bf7a17644c45db5953
shibahex <badamsva@gmail.com>
2026-04-27 20:18:11 -0400

    fix(build.sh): add OS detection for cross-platform sed compatibility

--- build.sh
+++ build.sh
@@ -8,14 +8,19 @@ new_dir="./out/next"
 
 # Rename the directory
 if [ -d "$old_dir" ]; then
-    mv "$old_dir" "$new_dir"
-    echo "Renamed $old_dir to $new_dir"
+  mv "$old_dir" "$new_dir"
+  echo "Renamed $old_dir to $new_dir"
 else
-    echo "Directory $old_dir does not exist."
-    exit 1
+  echo "Directory $old_dir does not exist."
+  exit 1
 fi
 
 # Update paths in HTML, CSS, and JS files
-find ./out -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" -o -name "*.json" \) -exec sed -i '' 's#/_next/#/next/#g' {} \;
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find ./out -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" -o -name "*.json" \) -exec sed -i '' 's#/_next/#/next/#g' {} \;
+else
+  find ./out -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" -o -name "*.json" \) -exec sed -i 's#/_next/#/next/#g' {} \;
+fi
 
 echo "Updated path references from /_next/ to /next/"
+
